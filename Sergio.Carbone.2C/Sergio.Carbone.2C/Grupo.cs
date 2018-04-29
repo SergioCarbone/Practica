baefbb5067a4b985c6d2e23ce016e3164c41aa45
SergioCarbone <sergio.carbone@gmail.com>
2018-04-29 15:33:54 -0300

    .

---
+++ Sergio.Carbone.2C/Sergio.Carbone.2C/Grupo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sergio.Carbone._2C
+{
+    public class Grupo
+    {
+        private List<Mascota> manada;
+        private string nombre;
+        private TipoManada tipo;
+
+        public TipoManada Tipo
+        {
+            set
+            {
+                this.tipo = TipoManada.Unica;
+            }
+        }
+
+        private Grupo()
+        {
+            manada = new List<Mascota>();
+        }
+
+        public Grupo(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+
+        public Grupo(string nombre, TipoManada tipo) : this(nombre)
+        {
+            this.tipo = tipo;
+        }
+        
+        public static bool operator ==(Grupo grupo, Mascota mascota)
+        {
+            bool retorno = false;
+            foreach (Mascota aux in grupo.manada)
+            {
+                if(aux == mascota)
+                {
+                    retorno = true;
+                }               
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Grupo grupo, Mascota mascota)
+        {
+            bool retorno = false;
+            foreach (Mascota aux in grupo.manada)
+            {
+                if (aux != mascota)
+                {
+                    retorno = true;
+                }                
+            }
+            return retorno;
+        }
+
+        public static Grupo operator +(Grupo grupo, Mascota mascota)
+        {
+            if(!(grupo == mascota))
+            {
+                grupo.manada.Add(mascota);
+            }
+            return grupo;
+        }
+
+        public static Grupo operator -(Grupo grupo, Mascota mascota)
+        {
+            if(grupo == mascota)
+            {
+                grupo.manada.Remove(mascota);
+            }
+            return grupo;
+        }
+
+        public static implicit operator string(Grupo grupo)
+        {
+            return grupo.manada.ToString();
+        }
+
+
+        public enum TipoManada
+        {
+            Unica,
+            Mixta,
+        }
+    }
+}
---
+++ Sergio.Carbone.2C/Sergio.Carbone.2C/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sergio.Carbone._2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mi central
+            Grupo c = new Grupo("Rio", Grupo.TipoManada.Unica);
+            Perro p1 = new Perro("Moro", "Pitbull");
+            Perro p2 = new Perro("Julio", "Cruza", 13,false);
+            Perro p3 = new Perro("Ramon", "Salchica", 2, true);
+            Gato g1 = new Gato("Jose", "Angora");
+            Gato g2 = new Gato("Hernan","Cruza");
+            Gato g3 = new Gato("Fer", "Siames");
+
+            
+            Console.WriteLine(c.ToString());
+           // c.Llamadas.Add(g1);
+          //  Console.WriteLine(c.ToString());
+           
+            Console.ReadKey();
+        }
+    }
+}
